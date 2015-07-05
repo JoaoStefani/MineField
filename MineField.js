7cc27a157708bfe04c06a3d5d1192deb22cc3b85
Lenon Bordini <lenonbordini@live.com>
2015-07-05 13:41:17 -0300

    v2 Minefield
    Add verify win

--- MineField.js
+++ MineField.js
@@ -1,9 +1,21 @@
 $(function () {
-    var gameEngine = (function () {
-        var _lines = 10, _columns = 10;
+    var randomColor = function () {
+        var randomColor = [];
+        for (var i = 0; i < 3; i++)
+            randomColor.push(Math.floor(Math.random() * 255) + 1);
+        return "rgb(" + randomColor.join(", ") + ")";
+    };
 
-        function Field(line, column) {
-            var sizeField = (100 / (_lines > _columns ? _lines : _columns)) + "%";
+    $("body").css("background", randomColor());
+
+    var gameEngine = (function () {
+        var _lines = 10,
+            _columns = 10,
+            _mines,
+            _intervalWin,
+            _key = [];
+
+        var Field = function (line, column, sizeField) {
             return $("<button/>", {
                 "class": "field",
                 "style": "width: " + sizeField + "; height: " + sizeField
@@ -11,9 +23,9 @@ $(function () {
                 "line": line,
                 "column": column,
                 "mine": false
-            });;
+            });
         };
-        function getField(line, column) {
+        var getField = function (line, column) {
             var field;
             $(".field").each(function () {
                 if ($(this).data("line") == line && $(this).data("column") == column) {
@@ -23,23 +35,67 @@ $(function () {
             });
             return field;
         };
-        function generateMines(percentageMines) {
-            var length = _lines * _columns / 100 * (percentageMines || 20);
+        var generateMines = function (percentageMines) {
+            _mines = _lines * _columns / 100 * (percentageMines || 20);
 
-            while (length > 0) {
-                var line = Math.floor(Math.random() * _lines),
-                    column = Math.floor(Math.random() * _columns);
+            var random = function (max) { return Math.floor(Math.random() * max); };
 
-                var field = getField(line, column);
+            while (_mines > 0) {
+                var field = getField(random(_lines), random(_columns));
 
-                if (field.data("mine"))
-                    continue;
+                if (field.data("mine")) continue;
 
                 field.data("mine", true);
-                length--;
+                _mines--;
             }
         };
-        function fieldsSiblings(f) {
+        var allMinesFound = function () {
+            var mines = $(".field").filter(function () {
+                return $(this).data("mine");
+            });
+            var minesFound = mines.filter(function () {
+                return $(this).hasClass("mineFound");
+            });
+            return mines.length == minesFound.length;
+        };
+        var allFieldsClean = function () {
+            var fieldsClean = $(".field").filter(function () {
+                return !$(this).data("mine");
+            });
+            var fieldsCleanFound = fieldsClean.filter(function () {
+                return $(this).hasClass("disabled");
+            });
+            return fieldsClean.length == fieldsCleanFound.length;
+        };
+        var backToStart = function (message) {
+            if (message) alert(message);
+            $(".field").prop("disabled", true);
+            $(".playScreen").removeClass("closed");
+        };
+
+        var prepareField = function (length, percentageMines) {
+            clearInterval(_intervalWin);
+            $(".mineCamp").removeClass("rotate").empty();
+
+            _lines = length;
+            _columns = length;
+
+            for (var l = 0; l < _lines; l++) {
+                var line = $("<div/>"),
+                    sizeField = (100 / (_lines > _columns ? _lines : _columns)) + "%";
+
+                for (var c = 0; c < _columns; c++)
+                    line.append(new Field(l, c, sizeField));
+
+                $(".mineCamp").append(line);
+            }
+
+            $(window).trigger("resize");
+            generateMines(percentageMines);
+
+            $(".playScreen").addClass("closed");
+        };
+        var fieldsSiblings = function (f) {
             var siblings = [];
             for (var l = f.data("line") - 1; l <= f.data("line") + 1; l++)
                 for (var c = f.data("column") - 1; c <= f.data("column") + 1; c++)
@@ -47,30 +103,7 @@ $(function () {
                         siblings.push(getField(l, c));
             return siblings;
         };
-
-        function prepareField(lineLength, columnLength, percentageMines) {
-            $(".mineCamp").empty();
-
-            _lines = lineLength;
-            _columns = columnLength;
-
-            for (var l = 0; l < _lines; l++) {
-                var line = $("<div/>");
-
-                for (var c = 0; c < _columns; c++)
-                    line.append(new Field(l, c));
-
-                $(".mineCamp").append(line);
-            }
-
-            $(".playScreen").fadeOut('fast', function () {
-                $(".mineCamp").fadeIn();
-            });
-
-            $(window).trigger("resize");
-            generateMines(percentageMines);
-        };
-        function minesArround(field) {
+        var minesArround= function (field) {
             var siblings = fieldsSiblings(field);
             var mines = siblings.filter(function (f) { return $(f).data("mine"); }).length;
             if (mines) return mines;
@@ -80,17 +113,63 @@ $(function () {
                     $(s).trigger("mousedown");
             });
         };
+        var winnerPlayer = function () {
+            if (allMinesFound() && allFieldsClean()) {
+                backToStart("You WIN!");
+                $(".mineCamp").addClass("rotate");
+                _intervalWin = setInterval(function () {
+                    $("body").css("background", randomColor());
+
+                    $(".field.mineFound").each(function () {
+                        $(this).css("background", "linear-gradient(" + randomColor() + ", " + randomColor() + ")");
+                    });
+                }, 200);
+            }
+        };
+        var gameOver = function () {
+            var colorBody = $("body").css("background");
+            $("body").css("background", "#ff0000");
+            setTimeout(function () {
+                $("body").css("background", colorBody);
+            }, 700);
+            backToStart();
+        };
+        var fieldHelp = function (f, key) {
+            $("*[class*='sibling'").removeClass("sibling1 sibling2 sibling3 sibling4 sibling5 sibling6 sibling7 sibling8");
+            if (key)
+                gameEngine.fieldSiblings(f).forEach(function (item, i) {
+                    $(item).addClass("sibling" + (i + 1));
+                });
+        };
+        var password = function (key) {
+            _key.push(key);
+
+            if (_key.join(".").indexOf("104.97.99.107") >= 0) {
+                _key = [];
+                alert("Hack ON");
+                $(".field").each(function () {
+                    if ($(this).data("mine"))
+                        $(this).addClass("mineFound");
+                });
+            } else if (_key.length > 10) _key.splice(0, 5);
+        };
 
         return {
             prepareField: prepareField,
-            minesArround: minesArround
+            minesArround: minesArround,
+            fieldSiblings: fieldsSiblings,
+            winnerPlayer: winnerPlayer,
+            gameOver: gameOver,
+            fieldHelp: fieldHelp,
+            password: password
         };
     })();
 
     $(document).on('contextmenu', function () { return false; });
 
-    $("#btnMineField").click(function () {
-        gameEngine.prepareField($("#lines").val(), $("#columns").val());
+    $("#formPlayGame").submit(function () {
+        gameEngine.prepareField($("#length").val());
+        return false;
     });
 
     $(window).resize(function () {
@@ -101,28 +180,52 @@ $(function () {
         $(".mineCamp").css({ "width": size });
     });
 
-    $(document).on("mousedown", ".field:not(.mineFound)", function (e) {
+    $(document).on("mousedown", ".field:not(.mineFound, .disabled)", function (e) {
         if (e.button != 0 && !e.isTrigger)
             return;
 
-        $(this).prop("disabled", true).removeClass("doubts");
+        $(this).addClass("disabled").removeClass("doubts");
 
         if ($(this).data("mine")) {
-            $(this).addClass("mine").prop("disabled", true);
-            $(".mineCamp").fadeOut('fast', function () {
-                $(".playScreen").fadeIn();
-            });
+            $(this).addClass("mine");
+            gameEngine.gameOver();
             return;
         }
 
         var minesArround = gameEngine.minesArround($(this));
         if (minesArround) $(this).addClass("has" + minesArround);
-    }).on("contextmenu", ".field", function () {
+
+        gameEngine.winnerPlayer();
+    });
+    $(document).on("contextmenu", ".field:not(.disabled)", function () {
         var field = $(this);
         switch (true) {
-            case field.hasClass("mineFound"): field.removeClass("mineFound").addClass("doubts"); return;
-            case field.hasClass("doubts"): field.removeClass("doubts").removeClass("mineFound doubts"); return;
-            default: field.addClass("mineFound"); return;
+            case field.hasClass("mineFound"):
+                field.removeClass("mineFound").addClass("doubts");
+                break;
+            case field.hasClass("doubts"):
+                field.removeClass("doubts").removeClass("mineFound doubts");
+                break;
+            default:
+                field.addClass("mineFound");
+                break;
+        }
+
+        gameEngine.winnerPlayer();
+    });
+    $(document).on("mousemove", ".field", function (e) {
+        gameEngine.fieldHelp($(this), e.ctrlKey);
+    });
+    $(document).on("keydown keyup", function (e) {
+        gameEngine.fieldHelp($(".field:hover"), e.ctrlKey);
+    }).on("keypress", function (e) {
+        gameEngine.password(e.which);
+
+        switch (e.which) {
+            case 99: $("body").css("background", randomColor()); return;
+            case 114: $("#formPlayGame").trigger("submit"); return;
+            case 109: $('.playScreen').toggleClass('closed'); return;
+            default: return;
         }
     });
 });
